5d8f8c541eef015fd476165dafd0eea2074289ef
Ravikesh Yadav <ravikesh@netzary.com>
2020-07-18 00:17:31 +0530

    Updates

---
+++ precache-manifest.5af7f84e561434ac52752cbc0f36315c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc0e9b7da743248b94bb2e11724a251",
+    "url": "/udemytodo/index.html"
+  },
+  {
+    "revision": "04ba8d645cf232e9f6f2",
+    "url": "/udemytodo/static/css/main.c082741d.chunk.css"
+  },
+  {
+    "revision": "d6351b1049ac9c66b194",
+    "url": "/udemytodo/static/js/2.94164886.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/udemytodo/static/js/2.94164886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04ba8d645cf232e9f6f2",
+    "url": "/udemytodo/static/js/main.b069dc77.chunk.js"
+  },
+  {
+    "revision": "cf6a99e373be203da71d",
+    "url": "/udemytodo/static/js/runtime-main.40278cc5.js"
+  }
+]);
